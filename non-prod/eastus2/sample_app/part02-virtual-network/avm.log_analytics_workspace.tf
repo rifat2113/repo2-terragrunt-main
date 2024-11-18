5e0e9e54b8ac56507b9570fdad50bf1c01b7ca78
rifat2113 <119580998+rifat2113@users.noreply.github.com>
2024-11-18 15:03:53 +0530

    Delete non-prod/eastus2/sample_app/part02-virtual-network directory

--- non-prod/eastus2/sample_app/part02-virtual-network/avm.log_analytics_workspace.tf
+++
@@ -1,9 +0,0 @@
-module "log_analytics_workspace" {
-  source  = "Azure/avm-res-operationalinsights-workspace/azurerm"
-  version = "0.3.5"
-
-  name                = local.log_analytics_workspace_name
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-  tags                = var.tags
-}
